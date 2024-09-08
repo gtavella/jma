cc2ed61cee304d8812d5779d0907fc01e6f5a6a8
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2024-09-08 18:46:54 +0200

    Update index.php

--- relation_facil/index.php
+++ relation_facil/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
 <div class="container">
     <div class="row mt-3">
         <h3>Fill in data</h3>
@@ -172,3 +182,140 @@
         <!-- Results content here -->
     </div>
 </div>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/project/assets/js/Entity.class.js"></script>
+
+<script>
+
+    var entity_manager = new EntityManager();
+
+    /**
+     * 
+     */
+    function toggle_input(input_id_toggle, input_id_truth) {
+        // hide/show the input to toggle
+        $("#"+input_id_toggle).toggle();
+        // fill the input to toggle with the value 
+        // from the source input
+        // for example, if you want to toggle the 
+        // plural fields, you now make the plural 
+        // field the same as the singular field
+        $("#"+input_id_toggle).val($("#"+input_id_truth).val());
+
+    }
+
+    /**
+     * Show phrases in the UI
+     */
+    function show_phrases(phrases) {
+
+        function make_checkbox(rel) {
+            let is_checked = false;
+            if (rel == "1A->1B" || rel == "1B->1A") {
+                is_checked = true;
+                entity_manager.compute_rels(rel);
+            }
+            return `
+                <input type="checkbox" value="${rel}" ${is_checked ? 'checked' : ''} onchange='show_result(this)'>
+            `;
+        }
+
+        var items = phrases.map(function(phrase) {
+            return `
+                <li>${phrase.phrase} ${make_checkbox(phrase.rel)}</li>
+            `;
+        }).join("");
+
+        $("#list_relationships").html(items);
+
+        
+    }
+
+
+    function show_result(html_checkbox) {
+        // current relationship
+        let curr_rel = $(html_checkbox).val()
+        let res = entity_manager.compute_rels(curr_rel);
+         
+    }
+
+    function compute_relationship(html_checkbox) {
+        console.log(html_checkbox);
+    }
+
+
+
+    function when_key_up() {
+        find_relationship(document.getElementById("form"));
+    }
+
+    function find_relationship(html_form) {
+        var $form = $(html_form);
+        const formData = new FormData(html_form);
+        // let areFieldsFilled = formData.has("commerciale") && formData.has("mese");
+        
+        // check if the user has not provided
+        // a commerciale
+        // if (!areFieldsFilled) {
+        //     alert("Inserisci tutti i campi");
+        //     return;
+        // }
+
+        // make an object with key value pairs
+        // like the form
+        var form = {};
+        formData.forEach((value, key) => {
+            form[key] = value;
+        });
+        
+        var symbols = {
+            "one": form["symbol_one"],
+            "many": form["symbol_many"]
+        };
+
+        var entityA_name = [
+            form["entityA_sing"],
+            form["entityA_plur"]
+        ];
+
+        var entityB_name = [
+            form["entityB_sing"],
+            form["entityB_plur"]
+        ];
+
+        var entityA_action = [
+            form["entityA_action_sing"],
+            // not using the plural of action
+            form["entityA_action_plur"]
+        ];
+
+        var entityB_action = [
+            form["entityB_action_sing"],
+            // not using the plural of action
+            form["entityB_action_plur"]
+        ];
+
+        entity_manager.empty();
+
+        entity_manager.set_symbols(symbols);
+
+        var entityA = entity_manager.new_entity(entityA_name, entityA_action);
+        var entityB = entity_manager.new_entity(entityB_name, entityB_action);
+
+        var phrases = entity_manager.generate_phrases();
+
+        show_phrases(phrases);
+
+    }
+
+
+
+</script>
+
+</body>
+</html>
